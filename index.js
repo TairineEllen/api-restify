8c9187b9f2a6db8e791f26e2cc9c7ff6fb2e2db0
TairineEllen <tairine.ellen@gmail.com>
2022-07-21 17:34:19 -0300

    create server with restify and add env

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv-safe').config();
+const restify = require('restify');
+const mongoose = require('mongoose');
+
+const server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.PORT, () => {
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.once('open', () => {
+  
+  console.log(`Server started on port ${process.env.PORT || 3000}`)
+})
