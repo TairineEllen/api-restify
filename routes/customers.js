865af11c435341ef0292609b63e67f72e2200ad2
TairineEllen <tairine.ellen@gmail.com>
2022-07-21 22:07:13 -0300

    add get method to fetch all customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,14 @@
 const errors = require('restify-errors');
+const Customer = require('../models/Customer');
 
 module.exports = server => {
-  server.get('/customers', (req, res, next) => {
-    res.send({ message: 'Test' });
-    next();
+  server.get('/customers', async (req, res, next) => {
+    try {
+      const customers = await Customer.find({});
+      res.send(customers);
+      next();
+    } catch(error) {
+      return next(new errors.InvalidContentError(error));
+    }
   });
 };
