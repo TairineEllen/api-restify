400993e4f1fe01be58035129dd6db30aff655084
TairineEllen <tairine.ellen@gmail.com>
2022-07-21 22:33:11 -0300

    add post method to create a customer

--- routes/customers.js
+++ routes/customers.js
@@ -11,4 +11,22 @@ module.exports = server => {
       return next(new errors.InvalidContentError(error));
     }
   });
+
+  server.post('/customers', async (req, res, next) => {
+    if (!req.is('application/json')) {
+      return next(new errors.InvalidContentError('Expects ""application/json"'));
+    }
+
+    const { name, email, balance } = req.body;
+
+    const customer = new Customer({ name, email, balance });
+
+    try {
+      await customer.save();
+      res.send(201);
+      next();
+    } catch(error) {
+      return next(new errors.InternalError(error.message));
+    }
+  })
 };
