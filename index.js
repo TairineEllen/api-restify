8075e42d837130d8f6a6c1bc9d43af904d2e22bb
TairineEllen <tairine.ellen@gmail.com>
2022-07-25 21:42:35 -0300

    add jwt authentication middleware

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv-safe').config();
 const restify = require('restify');
 const mongoose = require('mongoose');
+const rjwt = require('restify-jwt-community');
 
 const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser());
+
+server.use(rjwt({ secret: process.env.JWT_SECRET}).unless({ path: ['/auth'] }));
 
 server.listen(process.env.PORT, () => {
   mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
