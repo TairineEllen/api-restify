d49551c20ed2bd2b49611912bfe12e4150c7c9ec
TairineEllen <tairine.ellen@gmail.com>
2022-07-25 21:23:04 -0300

    create route to authenticate a user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const auth = require('../auth');
 
 module.exports = server => {
   server.post('/register', (req, res, next) => {
@@ -16,12 +17,22 @@ module.exports = server => {
           await user.save();
           res.send(201);
           next();
-        } catch(error) {
+        } catch (error) {
           return next(new errors.InternalError(error.message));
         }
-        
-      })
+      });
+    });
+  });
 
-    })
-  })
+  server.post('/auth', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await auth.authenticate(email, password);
+      console.log(user);
+      next();
+    } catch (error) {
+      return next(new errors.UnauthorizedError(error));
+    }
+  });
 }
